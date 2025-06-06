f5f1a6e5c2acb50cf1def9f1d79e1cf3dc3f0924
lushop22 <luis.ponce.sec@gmail.com>
2025-06-06 03:06:07 -0400

    Speakers added v1

---
+++ speakers.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.speaker-card').forEach(card => {
+card.addEventListener('click', function (e) {
+    // Evita que se active si se hizo clic en un <a> dentro del card
+    if (!e.target.closest('a')) {
+    const url = this.getAttribute('data-url');
+    if (url) window.location.href = url;
+    }
+});
+});
